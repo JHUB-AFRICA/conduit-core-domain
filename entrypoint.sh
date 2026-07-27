29bef44b63813a40fcb1588356355ecb42844a20
osama <osamaabdullahimohammed@gmail.com>
2026-07-27 11:39:17 +0300

    chore: reduce Gunicorn workers for deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ if [ "$DJANGO_ENV" = "production" ]; then
 
     exec gunicorn config.wsgi:application \
         --bind 0.0.0.0:8000 \
-        --workers=3 \
+        --workers=1 \
         --threads=2 \
         --timeout=120 \
         --access-logfile=- \
